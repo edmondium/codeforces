77d161362c00164490a9a6434bf65746abe6ad2c
edmondium <nicko@uin-suska.ac.id>
2026-03-24 21:55:32 +0700

    Codeforces Beta Round 1

--- TheatreSquare.cpp
+++ TheatreSquare.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
-int main(){
-    std::int64_t n, m, a;
-    std::cin >> n >> m >> a;
-    std::int64_t flagstones_needed = ((n + a - 1) / a) * ((m + a - 1) / a);
-    std::cout << "" << flagstones_needed << std::endl;
-    return 0;
+#include <bits/stdc++.h>
+#include <omp.h>
+using namespace std;
+
+int main() {
+    int64_t n, m, a;
+    cin >> n >> m >> a;
+
+    vector<int64_t> dims = {
+        (n + a - 1) / a,
+        (m + a - 1) / a
+    };
+
+    int64_t result = 1;
+    #pragma omp parallel for reduction(*:result)
+    for (int i = 0; i < (int)dims.size(); ++i)
+        result *= dims[i];
+
+    cout << result << '\n';
 }
