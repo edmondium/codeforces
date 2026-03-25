43db5e089bdf0ea3073e1f429ee17783d49ebef4
edmondium <nicko@uin-suska.ac.id>
2026-03-25 22:37:14 +0700

    Codeforces Beta Round 2

--- TheLeastRoundWay.cpp
+++ TheLeastRoundWay.cpp
@@ -1,91 +1,58 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
 constexpr int INF = 1e9;
 
-struct Cell {
-    int count;
-    char move;
+struct Cell { int count; char move; };
+
+auto factorCount = [](int num, int factor) {
+    int c = 0;
+    while (num && num % factor == 0) num /= factor, ++c;
+    return c;
 };
 
-pair<int, string> findPath(vector<vector<int>> &grid, int n) {
-    vector<vector<Cell>> dp2(n, vector<Cell>(n, {INF, ' '}));
-    vector<vector<Cell>> dp5(n, vector<Cell>(n, {INF, ' '}));
+pair<int,string> findPath(vector<vector<int>> &grid, int n) {
+    vector<vector<Cell>> dp2(n, vector<Cell>(n, {INF,' '}));
+    vector<vector<Cell>> dp5(n, vector<Cell>(n, {INF,' '}));
+    bool hasZero = false; int zx=-1, zy=-1;
+    dp2[0][0].count = factorCount(grid[0][0],2);
+    dp5[0][0].count = factorCount(grid[0][0],5);
 
-    auto factorCount = [](int num, int factor) {
-        int count = 0;
-        while (num && num % factor == 0) {
-            num /= factor;
-            count++;
-        }
-        return count;
-    };
-
-    bool hasZero = false;
-    int zeroX = -1, zeroY = -1;
-
-    dp2[0][0].count = factorCount(grid[0][0], 2);
-    dp5[0][0].count = factorCount(grid[0][0], 5);
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (grid[i][j] == 0) {
-                hasZero = true;
-                zeroX = i;
-                zeroY = j;
-            }
-            int twos = factorCount(grid[i][j], 2);
-            int fives = factorCount(grid[i][j], 5);
-
-            if (i > 0 && dp2[i - 1][j].count + twos < dp2[i][j].count) {
-                dp2[i][j] = {dp2[i - 1][j].count + twos, 'D'};
-            }
-            if (j > 0 && dp2[i][j - 1].count + twos < dp2[i][j].count) {
-                dp2[i][j] = {dp2[i][j - 1].count + twos, 'R'};
-            }
-
-            if (i > 0 && dp5[i - 1][j].count + fives < dp5[i][j].count) {
-                dp5[i][j] = {dp5[i - 1][j].count + fives, 'D'};
-            }
-            if (j > 0 && dp5[i][j - 1].count + fives < dp5[i][j].count) {
-                dp5[i][j] = {dp5[i][j - 1].count + fives, 'R'};
-            }
-        }
+    #pragma omp parallel for collapse(2) schedule(dynamic)
+    for (int i=0;i<n;i++) for (int j=0;j<n;j++) {
+        if (grid[i][j]==0) { hasZero=true; zx=i; zy=j; }
+        int twos=factorCount(grid[i][j],2), fives=factorCount(grid[i][j],5);
+        if (i>0 && dp2[i-1][j].count+twos<dp2[i][j].count) dp2[i][j]={dp2[i-1][j].count+twos,'D'};
+        if (j>0 && dp2[i][j-1].count+twos<dp2[i][j].count) dp2[i][j]={dp2[i][j-1].count+twos,'R'};
+        if (i>0 && dp5[i-1][j].count+fives<dp5[i][j].count) dp5[i][j]={dp5[i-1][j].count+fives,'D'};
+        if (j>0 && dp5[i][j-1].count+fives<dp5[i][j].count) dp5[i][j]={dp5[i][j-1].count+fives,'R'};
     }
 
-    int minZeros = min(dp2[n - 1][n - 1].count, dp5[n - 1][n - 1].count);
+    int minZeros=min(dp2[n-1][n-1].count,dp5[n-1][n-1].count);
     string path;
-
-    if (hasZero && minZeros > 1) {
-        minZeros = 1;
-        for (int i = 0; i < zeroX; ++i) path += 'D';
-        for (int j = 0; j < zeroY; ++j) path += 'R';
-        for (int i = zeroX; i < n - 1; ++i) path += 'D';
-        for (int j = zeroY; j < n - 1; ++j) path += 'R';
+    if (hasZero && minZeros>1) {
+        minZeros=1;
+        path.append(zx,'D'); path.append(zy,'R');
+        path.append(n-1-zx,'D'); path.append(n-1-zy,'R');
     } else {
-        auto &trace = (dp2[n - 1][n - 1].count < dp5[n - 1][n - 1].count) ? dp2 : dp5;
-        int x = n - 1, y = n - 1;
-        while (x > 0 || y > 0) {
-            path += trace[x][y].move;
-            if (trace[x][y].move == 'D') x--;
-            else y--;
+        auto &trace=(dp2[n-1][n-1].count<dp5[n-1][n-1].count)?dp2:dp5;
+        int x=n-1,y=n-1;
+        while (x>0||y>0) {
+            path+=trace[x][y].move;
+            if (trace[x][y].move=='D') --x; else --y;
         }
-        reverse(path.begin(), path.end());
+        reverse(path.begin(),path.end());
     }
-
-    return {minZeros, path};
+    return {minZeros,path};
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<vector<int>> grid(n, vector<int>(n));
-
-    for (auto &row : grid)
-        for (auto &cell : row)
-            cin >> cell;
-
-    auto [minZeros, path] = findPath(grid, n);
-    cout << minZeros << endl << path << endl;
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin>>n;
+    vector<vector<int>> grid(n,vector<int>(n));
+    for (auto &row:grid) for (auto &cell:row) cin>>cell;
+    auto [minZeros,path]=findPath(grid,n);
+    cout<<minZeros<<"\n"<<path<<"\n";
 }
