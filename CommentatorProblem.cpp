4f2d9a78b6edafbbe929e58266dd39a8d8faccfc
edmondium <nicko@uin-suska.ac.id>
2026-03-25 22:52:47 +0700

    Codeforces Beta Round 2

--- CommentatorProblem.cpp
+++ CommentatorProblem.cpp
@@ -1,67 +1,44 @@
-#include <cmath>
-#include <iostream>
-#include <iomanip>
+#include <bits/stdc++.h>
+#include <omp.h>
+using namespace std;
 
-const double eps=1e-5;
-struct point
-{
-    double x, y, r;
-}p[3];
-double dist(double x, double y, point tp){
-    return sqrt((x-tp.x)*(x-tp.x)+(y-tp.y)*(y-tp.y));
+struct Point { double x,y,r; };
+array<Point,3> p;
+constexpr double eps=1e-5;
+
+double dist(double x,double y,const Point& q){
+    return hypot(x-q.x,y-q.y);
 }
-double cost(double x, double y){
-    double ang[3];
- 
-    for(int i=0; i<3; ++i){
-        ang[i]=dist(x, y, p[i])/p[i].r; //sin
-    }
-    double d[3];
-    for(int i=0; i<3; ++i){
-        d[i]=ang[i]-ang[(i+1)%3]; //the difference between the angles of view
-    }
+
+double cost(double x,double y){
+    array<double,3> ang;
+    for(int i=0;i<3;i++) ang[i]=dist(x,y,p[i])/p[i].r;
     double ans=0;
-    for(int i=0; i<3; ++i){
-                 ans += d[i]*d[i] ; //similar variance
+    for(int i=0;i<3;i++){
+        double d=ang[i]-ang[(i+1)%3];
+        ans+=d*d;
     }
     return ans;
 }
-int main(){
- 
-    for(int i=0; i<3; i++){
-        std::cin >> p[i].x >> p[i].y >> p[i].r;
 
-    }
-    double x=0, y=0;
-    for (int i = 0; i < 3; ++i)
-    {
-                 x += p[i].x/3;
-                 y += p[i].y/3; //Search in the center of gravity attachment
-    }
+int main(){
+    for(auto& q:p) cin>>q.x>>q.y>>q.r;
+    double x=accumulate(p.begin(),p.end(),0.0,[](double acc,const Point& q){return acc+q.x;})/3.0;
+    double y=accumulate(p.begin(),p.end(),0.0,[](double acc,const Point& q){return acc+q.y;})/3.0;
     double t=1.0;
     while(t>eps){
-        int kase=0;
         double tp=cost(x,y);
-        if(cost(x+t,y)<tp){
-            x+=t;
-            kase=1;
-        }else if(cost(x-t,y)<tp){
-            x-=t;
-            kase=1;
-        }else if(cost(x,y-t)<tp){
-            y-=t;
-            kase=1;
-        }else if(cost(x,y+t)<tp){
-            y+=t;
-            kase=1;
+        bool kase=false;
+        array<pair<double,double>,4> dirs={{{t,0},{-t,0},{0,-t},{0,t}}};
+        #pragma omp parallel for
+        for(int i=0;i<4;i++){
+            double nx=x+dirs[i].first, ny=y+dirs[i].second;
+            if(cost(nx,ny)<tp){
+                #pragma omp critical
+                { x=nx; y=ny; kase=true; }
+            }
         }
-                 if (!kase) //No better solutions around the world
-        {
-            t /= 2;
-        }
+        if(!kase) t/=2;
     }
-        if(fabs(cost(x,y)<eps)){
-            std::cout << std::fixed << std::setprecision(5) << x << " " << y << std::endl;
-        }   
-    return 0;
+    if(fabs(cost(x,y))<eps) cout<<fixed<<setprecision(5)<<x<<" "<<y<<"\n";
 }
