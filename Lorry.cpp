ead97ce7b9b220c26d6c2d137220022db49131a4
edmondium <nicko@uin-suska.ac.id>
2026-03-26 17:33:26 +0700

    Codeforces Beta Round 3

--- Lorry.cpp
+++ Lorry.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstdint>
+#include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
 int main() {
@@ -9,73 +7,36 @@ int main() {
     cin.tie(nullptr);
 
     int n;
-    // Use a 64-bit variable for v because v can be as large as 10^9.
     int64_t v;
     cin >> n >> v;
 
-    // We will store pairs (capacity, index). There are two groups: type==1 (kayaks) and type==2 (catamarans)
-    vector<pair<int, int>> kayaks;      // (p, original_index)
-    vector<pair<int, int>> catamarans;  // (p, original_index)
-
-    for (int i = 0; i < n; i++) {
-        int t, p;
-        cin >> t >> p;
-        if (t == 1) {
-            kayaks.push_back({p, i + 1});
-        } else {
-            catamarans.push_back({p, i + 1});
-        }
-    }
-
-    // Sort both lists in descending order (largest p first)
-    sort(kayaks.begin(), kayaks.end(), [](const pair<int,int>& a, const pair<int,int>& b) {
-        return a.first > b.first;
-    });
-    sort(catamarans.begin(), catamarans.end(), [](const pair<int,int>& a, const pair<int,int>& b) {
-        return a.first > b.first;
+    vector<pair<int,int>> kayaks, catamarans;
+    ranges::for_each(views::iota(0,n), [&](int i){
+        int t,p; cin>>t>>p;
+        (t==1?kayaks:catamarans).emplace_back(p,i+1);
     });
 
-    // Precompute prefix sums to quickly sum the best x waterborne vehicles from each list.
-    int kayakSize = kayaks.size(), catSize = catamarans.size();
-    vector<int64_t> prefixKayaks(kayakSize + 1, 0);
-    vector<int64_t> prefixCat(catSize + 1, 0);
-    
-    for (int i = 0; i < kayakSize; i++) {
-        prefixKayaks[i + 1] = prefixKayaks[i] + kayaks[i].first;
+    ranges::sort(kayaks,greater{},&pair<int,int>::first);
+    ranges::sort(catamarans,greater{},&pair<int,int>::first);
+
+    vector<int64_t> prefixKayaks(kayaks.size()+1), prefixCat(catamarans.size()+1);
+    ranges::for_each(views::iota(0,(int)kayaks.size()),[&](int i){prefixKayaks[i+1]=prefixKayaks[i]+kayaks[i].first;});
+    ranges::for_each(views::iota(0,(int)catamarans.size()),[&](int i){prefixCat[i+1]=prefixCat[i]+catamarans[i].first;});
+
+    int bestCat=0,bestKayak=0;
+    int64_t bestTotal=0;
+
+    #pragma omp parallel for
+    for(int x=0;x<=catamarans.size();x++){
+        int64_t used=2LL*x;
+        if(used>v) continue;
+        int y=min<int64_t>(kayaks.size(),v-used);
+        int64_t total=prefixCat[x]+prefixKayaks[y];
+        #pragma omp critical
+        if(total>bestTotal){bestTotal=total;bestCat=x;bestKayak=y;}
     }
-    for (int i = 0; i < catSize; i++) {
-        prefixCat[i + 1] = prefixCat[i] + catamarans[i].first;
-    }
-    
-    // Now decide how many catamarans (each takes 2 cubic metres) to take.
-    // For each possible count, fill the remaining capacity with the best kayaks.
-    int bestCatCount = 0, bestKayakCount = 0;
-    int64_t bestTotal = 0;
-    
-    // x is the number of catamarans taken.
-    for (int x = 0; x <= catSize; x++) {
-        // All catamarans take 2*x volume.
-        int64_t usedVolume = 2LL * x;
-        if (usedVolume > v)
-            break;
-        // The remaining capacity can hold that many kayaks (each occupies 1).
-        int y = min((int64_t)kayakSize, v - usedVolume);
-        int64_t currentTotal = prefixCat[x] + prefixKayaks[y];
-        if (currentTotal > bestTotal) {
-            bestTotal = currentTotal;
-            bestCatCount = x;
-            bestKayakCount = y;
-        }
-    }
-    
-    // Output the optimal total carrying capacity.
-    cout << bestTotal << "\n";
-    // Output the indices of the vehicles making up the optimal set.
-    // Here the catamarans (first bestCatCount) and then kayaks (first bestKayakCount) from the sorted order.
-    for (int i = 0; i < bestCatCount; i++)
-        cout << catamarans[i].second << " ";
-    for (int i = 0; i < bestKayakCount; i++)
-        cout << kayaks[i].second << " ";
-    
-    return 0;
+
+    cout<<bestTotal<<"\n";
+    ranges::for_each(views::iota(0,bestCat),[&](int i){cout<<catamarans[i].second<<" ";});
+    ranges::for_each(views::iota(0,bestKayak),[&](int i){cout<<kayaks[i].second<<" ";});
 }
