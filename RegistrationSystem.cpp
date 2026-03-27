6ce097b6c77e2a8b21fa3eadb61ad0d7b61ab286
edmondium <nicko@uin-suska.ac.id>
2026-03-27 14:57:54 +0700

    Codeforces Beta Round 4

--- RegistrationSystem.cpp
+++ RegistrationSystem.cpp
@@ -1,38 +1,34 @@
-#include <iostream>
-#include <unordered_map>
-#include <string>
+#include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int n;
     cin >> n;
-    
-    unordered_map<string, int> db;
-    while (n--) {
-        string name;
-        cin >> name;
-        // If the name is new, output "OK" and mark it as seen.
-        if (db[name] == 0) {
-            cout << "OK" << "\n";
-            db[name] = 1;
-        } else {
-            // Otherwise, generate a new username by appending the smallest available number.
-            int count = db[name];
-            string newName = name + to_string(count);
-            // Although logically, this inner loop is not normally needed thanks to our counting strategy,
-            // we check in case a generated name has been added earlier independently.
-            while (db[newName] != 0) {
-                count++;
-                newName = name + to_string(count);
+    vector<string> names(n);
+    for (int i = 0; i < n; i++) cin >> names[i];
+
+    unordered_map<string,int> db;
+    vector<string> results(n);
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        string name = names[i];
+        #pragma omp critical
+        {
+            if (db[name] == 0) {
+                results[i] = "OK";
+                db[name] = 1;
+            } else {
+                string newName = name + to_string(db[name]++);
+                results[i] = newName;
+                db[newName] = 1;
             }
-            // Update the base name count and mark the new name as registered.
-            db[name] = count + 1;
-            db[newName] = 1;
-            cout << newName << "\n";
         }
     }
-    return 0;
+
+    for (auto &r : results) cout << r << '\n';
 }
