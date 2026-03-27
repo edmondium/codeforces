4b3c4ef070d9ec7e4fe515d56aa43ad0e6fd2b95
edmondium <nicko@uin-suska.ac.id>
2026-03-27 14:46:11 +0700

    Codeforces Beta Round 4

--- BeforeAnExam.cpp
+++ BeforeAnExam.cpp
@@ -1,41 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 #include <algorithm>
+#include <execution>
+#include <ranges>
+#include <omp.h>
 using namespace std;
 
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int d, sumTime;
     cin >> d >> sumTime;
     vector<int> minTime(d), maxTime(d);
-    
+
+    for (int i = 0; i < d; i++) cin >> minTime[i] >> maxTime[i];
+
     int totalMin = 0, totalMax = 0;
+
+    #pragma omp parallel for reduction(+:totalMin,totalMax)
     for (int i = 0; i < d; i++) {
-        cin >> minTime[i] >> maxTime[i];
         totalMin += minTime[i];
         totalMax += maxTime[i];
     }
-    
-    // Check if the sumTime is possible with given bounds
-    if(sumTime < totalMin || sumTime > totalMax){
-        cout << "NO";
+
+    if (sumTime < totalMin || sumTime > totalMax) {
+        cout << "NO\n";
         return 0;
     }
-    
-    // Construct a solution starting with all minimums
+
     vector<int> schedule = minTime;
     int extra = sumTime - totalMin;
-    
-    // Distribute the extra hours across days without surpassing maxTime
-    for(int i = 0; i < d && extra > 0; i++){
+
+    for (int i = 0; i < d && extra > 0; i++) {
         int add = min(extra, maxTime[i] - minTime[i]);
         schedule[i] += add;
         extra -= add;
     }
-    
-    cout << "YES" << "\n";
-    for (int i = 0; i < d; i++){
-        cout << schedule[i] << (i+1 < d ? " " : "\n");
-    }
-    
+
+    cout << "YES\n";
+    ranges::for_each(schedule | views::enumerate, [&](auto&& p) {
+        auto [i, val] = p;
+        cout << val << (i + 1 < d ? " " : "\n");
+    });
+
     return 0;
 }
