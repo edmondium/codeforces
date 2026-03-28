715fc01a757ac2777bd4f614456c285d0330f01f
edmondium <nicko@uin-suska.ac.id>
2026-03-28 10:00:13 +0700

    Codeforces Beta Round 5

--- CenterAlignment.cpp
+++ CenterAlignment.cpp
@@ -1,54 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     vector<string> lines;
     string s;
     int maxLength = 0;
-
-    // Read all lines from the input
-    while(getline(cin, s)) {
+    while (getline(cin, s)) {
         lines.push_back(s);
         maxLength = max(maxLength, (int)s.size());
     }
-    
-    // Prepare the top/bottom border
+
     string border(maxLength + 2, '*');
     cout << border << "\n";
-    
-    bool toggle = false; // false means extra space goes on the right, true means extra space goes on the left
-    for(auto &line : lines) {
-        int gap = maxLength - line.size();
+
+    vector<string> output(lines.size());
+    bool toggle = false;
+
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < (int)lines.size(); i++) {
+        int gap = maxLength - (int)lines[i].size();
         int leftSpaces, rightSpaces;
-        
-        if(gap % 2 == 0) {
-            // Even difference: symmetric
+        if (gap % 2 == 0) {
             leftSpaces = rightSpaces = gap / 2;
         } else {
-            // Odd difference: alternate allocation.
-            if(!toggle) {
-                // Start with extra space on the right: left gets floor(gap/2)
+            bool localToggle;
+            #pragma omp critical
+            {
+                localToggle = toggle;
+                toggle = !toggle;
+            }
+            if (!localToggle) {
                 leftSpaces = gap / 2;
                 rightSpaces = gap - leftSpaces;
             } else {
-                // Afterwards, extra space on the left: left gets ceil(gap/2)
                 leftSpaces = gap - gap / 2;
                 rightSpaces = gap / 2;
             }
-            toggle = !toggle;
         }
-        cout << "*" 
-             << string(leftSpaces, ' ') << line << string(rightSpaces, ' ')
-             << "*" << "\n";
+        output[i] = "*" + string(leftSpaces, ' ') + lines[i] + string(rightSpaces, ' ') + "*";
     }
+
+    for (auto &line : output) cout << line << "\n";
     cout << border;
-    
-    return 0;
 }
