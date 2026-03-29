6f1104de76385fddf6f8ed257b4f4e9310676340
edmondium <nicko@uin-suska.ac.id>
2026-03-29 15:29:37 +0700

    Codeforces Beta Round 6

--- Exposition.cpp
+++ Exposition.cpp
@@ -1,56 +1,61 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     long long k;
     cin >> n >> k;
-
     vector<long long> h(n);
-    for(int i = 0; i < n; i++)
-        cin >> h[i];
+    for (int i = 0; i < n; i++) cin >> h[i];
 
-    deque<int> maxDQ, minDQ;  // store indices of potential max / min in current window
-    int l = 0;                // left bound of window
-    int best = 0;             // best window length found so far
+    int best = 0;
     vector<pair<int,int>> ans;
 
-    for(int r = 0; r < n; r++){
-        // 1) Extend right end: maintain monotonicity
-        while(!maxDQ.empty() && h[maxDQ.back()] <= h[r])
-            maxDQ.pop_back();
-        maxDQ.push_back(r);
+    #pragma omp parallel
+    {
+        int local_best = 0;
+        vector<pair<int,int>> local_ans;
+        deque<int> maxDQ, minDQ;
+        int l = 0;
 
-        while(!minDQ.empty() && h[minDQ.back()] >= h[r])
-            minDQ.pop_back();
-        minDQ.push_back(r);
-
-        // 2) Shrink from left while invalid (max - min > k)
-        while(h[maxDQ.front()] - h[minDQ.front()] > k){
-            if(maxDQ.front() == l) maxDQ.pop_front();
-            if(minDQ.front() == l) minDQ.pop_front();
-            l++;
+        #pragma omp for schedule(dynamic)
+        for (int r = 0; r < n; r++) {
+            while (!maxDQ.empty() && h[maxDQ.back()] <= h[r]) maxDQ.pop_back();
+            maxDQ.push_back(r);
+            while (!minDQ.empty() && h[minDQ.back()] >= h[r]) minDQ.pop_back();
+            minDQ.push_back(r);
+            while (h[maxDQ.front()] - h[minDQ.front()] > k) {
+                if (maxDQ.front() == l) maxDQ.pop_front();
+                if (minDQ.front() == l) minDQ.pop_front();
+                l++;
+            }
+            int len = r - l + 1;
+            if (len > local_best) {
+                local_best = len;
+                local_ans.clear();
+                local_ans.emplace_back(l + 1, r + 1);
+            } else if (len == local_best) {
+                local_ans.emplace_back(l + 1, r + 1);
+            }
         }
 
-        // 3) Record current window [l..r]
-        int len = r - l + 1;
-        if(len > best){
-            best = len;
-            ans.clear();
-            ans.emplace_back(l + 1, r + 1);  // convert to 1-based
-        }
-        else if(len == best){
-            ans.emplace_back(l + 1, r + 1);
+        #pragma omp critical
+        {
+            if (local_best > best) {
+                best = local_best;
+                ans = move(local_ans);
+            } else if (local_best == best) {
+                ans.insert(ans.end(), local_ans.begin(), local_ans.end());
+            }
         }
     }
 
-    // Output
     cout << best << " " << ans.size() << "\n";
-    for(auto [L, R] : ans)
-        cout << L << " " << R << "\n";
-
-    return 0;
+    ranges::for_each(ans, [](auto &p) {
+        cout << p.first << " " << p.second << "\n";
+    });
 }
