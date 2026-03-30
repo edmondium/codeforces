9cfa1b8700c9713ee43a95795960d4c7e7249a18
edmondium <nicko@uin-suska.ac.id>
2026-03-30 17:44:45 +0700

    Codeforces Beta Round 7

--- PalindromeDegree.cpp
+++ PalindromeDegree.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
+
 using ull = unsigned long long;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     string s;
     cin >> s;
     int n = s.size();
-    vector<int> dp(n+1, 0);
-    // rolling‐hash parameters
-    const ull B = 1315423911ULL;  
-    ull hf = 0, hr = 0, powB = 1;
 
+    vector<int> dp(n + 1);
+    const ull B = 1315423911ULL;
+    ull hf = 0, hr = 0, powB = 1;
     long long answer = 0;
-    for(int i = 1; i <= n; i++){
-        unsigned char c = s[i-1];
-        // update forward and reverse hashes
+
+    for (int i = 1; i <= n; i++) {
+        unsigned char c = s[i - 1];
         hf = hf * B + c;
         hr = hr + powB * c;
         powB = powB * B;
-
-        // if full prefix is palindrome
-        if(hf == hr){
-            dp[i] = dp[i/2] + 1;
-        }
-        // else dp[i] stays 0
+        dp[i] = (hf == hr) ? dp[i / 2] + 1 : 0;
         answer += dp[i];
     }
 
-    cout << answer << "\n";
-    return 0;
+    long long total = 0;
+    #pragma omp parallel for reduction(+:total)
+    for (int i = 1; i <= n; i++) {
+        total += dp[i];
+    }
+
+    cout << total << "\n";
 }
