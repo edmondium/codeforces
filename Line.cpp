7b8817b01d1ad7434c91009ecffb2e6305ebbaac
edmondium <nicko@uin-suska.ac.id>
2026-03-30 17:33:31 +0700

    Codeforces Beta Round 7

--- Line.cpp
+++ Line.cpp
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 using ll = long long;
 
-// extended Euclid: returns (x,y,d) such that A*x + B*y = d = gcd(A,B)
-tuple<ll,ll,ll> ext_gcd(ll A, ll B) {
-    if (B==0) return { 1, 0, A };
-    auto [x1,y1,d] = ext_gcd(B, A % B);
-    ll x = y1;
-    ll y = x1 - (A / B) * y1;
-    return { x, y, d };
-}
+auto ext_gcd = [](auto self, ll A, ll B) -> tuple<ll,ll,ll> {
+    if (B==0) return {1,0,A};
+    auto [x1,y1,d] = self(self,B,A%B);
+    return tuple<ll,ll,ll>{y1, x1-(A/B)*y1, d};
+};
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     ll A,B,C;
     cin >> A >> B >> C;
-
-    // Solve A*x + B*y = -C
-    auto [x0,y0,g] = ext_gcd(llabs(A), llabs(B));
-    // adjust signs if A or B negative
-    if (A < 0) x0 = -x0;
-    if (B < 0) y0 = -y0;
-
-    ll rhs = -C;
-    if (rhs % g != 0) {
-        cout << "-1\n";
+    auto [x0,y0,g] = ext_gcd(ext_gcd,llabs(A),llabs(B));
+    if (A<0) x0=-x0;
+    if (B<0) y0=-y0;
+    ll rhs=-C;
+    if (rhs%g!=0) {
+        cout << -1 << "\n";
         return 0;
     }
-
-    ll mul = rhs / g;
-    ll x = x0 * mul;
-    ll y = y0 * mul;
-
+    ll mul=rhs/g;
+    ll x,y;
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        { x=x0*mul; }
+        #pragma omp section
+        { y=y0*mul; }
+    }
     cout << x << " " << y << "\n";
-    return 0;
 }
