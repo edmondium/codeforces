d5ac43767f9ea79fcafc1124ae2be578c5e74c08
edmondium <nicko@uin-suska.ac.id>
2026-04-01 15:26:06 +0700

    Codeforces Beta Round 9

--- DieRoll.cpp
+++ DieRoll.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
 int main() {
-    int Y, W;
-    cin >> Y >> W;
-    int m = max(Y, W);
-    int favorable = 6 - m + 1;
-    int total     = 6;
-    int g = gcd(favorable, total);
-    cout << (favorable / g) << '/' << (total / g) << "\n";
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int y, w;
+    cin >> y >> w;
+    int m = max(y, w);
+    int num = 6 - m + 1, den = 6;
+    int g;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        g = gcd(num, den);
+    }
+    cout << num / g << '/' << den / g << '\n';
 }
