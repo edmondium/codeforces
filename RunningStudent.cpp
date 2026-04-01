a33d7d7620783907bd1bdc2516c4d1e1b5278d70
edmondium <nicko@uin-suska.ac.id>
2026-04-01 15:36:00 +0700

    Codeforces Beta Round 9

--- RunningStudent.cpp
+++ RunningStudent.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
 int main() {
@@ -10,34 +11,25 @@ int main() {
     cin >> n >> vb >> vs;
 
     vector<double> x(n);
-    for (int i = 0; i < n; i++) {
-        cin >> x[i];
-    }
+    for (int i = 0; i < n; i++) cin >> x[i];
 
     double xu, yu;
     cin >> xu >> yu;
 
     const double EPS = 1e-9;
-    int bestIdx = 2;                 // We'll start checking from stop 2
-    // Compute for stop 2 as initial best
-    double dx = xu - x[1];
-    double bestRun = sqrt(dx*dx + yu*yu);
-    double bestTime = x[1] / vb + bestRun / vs;
+    vector<pair<double,int>> results(n);
 
-    for (int i = 2; i < n; i++) {
+    #pragma omp parallel for
+    for (int i = 1; i < n; i++) {
         double t_bus = x[i] / vb;
-        double dx_i = xu - x[i];
-        double run_dist = sqrt(dx_i*dx_i + yu*yu);
-        double t_tot = t_bus + run_dist / vs;
-
-        if (t_tot + EPS < bestTime
-            || (fabs(t_tot - bestTime) < EPS && run_dist < bestRun)) {
-            bestTime = t_tot;
-            bestRun  = run_dist;
-            bestIdx  = i + 1;  // convert 0-based to 1-based
-        }
+        double dx = xu - x[i];
+        double run = sqrt(dx*dx + yu*yu);
+        double t_tot = t_bus + run / vs;
+        results[i] = {t_tot * (1 + EPS) + run * 1e-12, i+1};
     }
 
-    cout << bestIdx << "\n";
-    return 0;
+    auto best = ranges::min_element(results | views::drop(1),
+        [](auto a, auto b){ return a.first < b.first; });
+
+    cout << best->second << '\n';
 }
