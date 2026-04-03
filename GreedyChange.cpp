10c1098ac8d971984c4d2335c01c7b42e13ab51b
edmondium <nicko@uin-suska.ac.id>
2026-04-03 20:57:55 +0700

    Codeforces Beta Round 10

--- GreedyChange.cpp
+++ GreedyChange.cpp
@@ -1,62 +1,44 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 using ll = long long;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     cin >> n;
     vector<ll> a(n);
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-    // Input guarantees a sorted descending and a[n-1] == 1.
-    // If not, uncomment next line:
-    // sort(a.begin(), a.end(), greater<ll>());
+    for (int i = 0; i < n; i++) cin >> a[i];
 
-    auto greedyCount = [&](ll x)->ll {
-        ll cnt = 0;
-        for(int i = 0; i < n; i++){
-            cnt += x / a[i];
-            x %= a[i];
-            if(x == 0) break;
-        }
-        return cnt;
+    auto greedyCount = [&](ll x) {
+        return accumulate(a.begin(), a.end(), 0LL, [&](ll cnt, ll coin) {
+            ll use = x / coin;
+            x %= coin;
+            return cnt + use;
+        });
     };
 
     ll best = LLONG_MAX;
-    // Try each pair (i,j): we’ll construct sum S = (a[i-1]-1 greedy up to j) + a[j]
-    for(int i = 1; i < n; i++){
-        // we're interested in breaking at coin value a[i-1]
-        for(int j = i; j < n; j++){
-            ll need = a[i-1] - 1;
-            ll sum = 0;
-            ll cnt = 0;
-            // greedy on coins a[0..j] to represent (a[i-1]-1)
-            for(int k = 0; k <= j && need > 0; k++){
+
+    #pragma omp parallel for reduction(min:best) schedule(dynamic)
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            ll need = a[i - 1] - 1;
+            ll sum = 0, cnt = 0;
+            for (int k = 0; k <= j && need > 0; k++) {
                 ll use = need / a[k];
                 cnt += use;
                 sum += use * a[k];
                 need -= use * a[k];
             }
-            // now add one more coin of a[j]
             sum += a[j];
             cnt++;
-
-            // compare to true greedy on the full set
             ll g = greedyCount(sum);
-            if(cnt < g){
-                best = min(best, sum);
-            }
+            if (cnt < g) best = min(best, sum);
         }
     }
 
-    if(best == LLONG_MAX) 
-        cout << -1 << "\n";
-    else
-        cout << best << "\n";
-
-    return 0;
+    cout << (best == LLONG_MAX ? -1 : best) << "\n";
 }
