0de765cb82b05401d6cca0e564cbdf59a7ea8703
edmondium <nicko@uin-suska.ac.id>
2026-04-04 13:35:30 +0700

    Codeforces Beta Round 11

--- IncreasingSequence.cpp
+++ IncreasingSequence.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
+#include <ranges>
+#include <omp.h>
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -12,10 +14,11 @@ int main(){
     for (auto &x : b) cin >> x;
 
     long long moves = 0;
+
+    #pragma omp parallel for reduction(+:moves)
     for (int i = 1; i < n; i++) {
         if (b[i] <= b[i-1]) {
             long long need = b[i-1] - b[i];
-            // how many +d steps to strictly exceed prev?
             long long k = need / d + 1;
             moves += k;
             b[i] += k * d;
@@ -23,5 +26,4 @@ int main(){
     }
 
     cout << moves << "\n";
-    return 0;
 }
