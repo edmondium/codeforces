4277c6124883207af9a1197398a558f1d901b56a
edmondium <nicko@uin-suska.ac.id>
2026-04-05 22:34:35 +0700

    Codeforces Beta Round 12

--- StartOfTheSeason.cpp
+++ StartOfTheSeason.cpp
@@ -1,29 +1,35 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    
-    int M[n][n];
-    
-    for(int i = 0;i < n - 1;++i)
-        for(int j = 0;j < n - 1;++j)
-            M[i][j] = 1 + (i + j) % (n - 1);
-    
-    for(int i = 0;i < n - 1;++i)
-        M[i][n - 1] = M[n - 1][i] = M[i][i];
-    
-    for(int i = 0;i < n;++i)
-        M[i][i] = 0;
-    
-    for(int i = 0;i < n;++i){
-        for(int j = 0;j < n;++j) cout << M[i][j] << ' ';
+
+    int* M = new int[n * n];
+
+    #pragma acc parallel loop collapse(2) copyout(M[0:n*n])
+    for (int i = 0; i < n - 1; ++i)
+        for (int j = 0; j < n - 1; ++j)
+            M[i * n + j] = 1 + (i + j) % (n - 1);
+
+    #pragma acc parallel loop copy(M[0:n*n])
+    for (int i = 0; i < n - 1; ++i) {
+        M[i * n + (n - 1)] = M[i * n + i];
+        M[(n - 1) * n + i] = M[i * n + i];
+    }
+
+    #pragma acc parallel loop copy(M[0:n*n])
+    for (int i = 0; i < n; ++i)
+        M[i * n + i] = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j)
+            cout << M[i * n + j] << ' ';
         cout << '\n';
     }
-    
-    return 0;
+
+    delete[] M;
 }
