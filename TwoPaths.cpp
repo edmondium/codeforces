4f51707436730fdeae3f72703cce4b333ba67fe6
edmondium <nicko@uin-suska.ac.id>
2026-04-07 12:42:59 +0700

    Codeforces Beta Round 14

--- TwoPaths.cpp
+++ TwoPaths.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static int n;
-static vector<int> adj[200];
-static int parentArr[200];
-static bool inPath[200], compVis[200];
-static int distArr[200], dist2Arr[200];
-static int q[200], q2[200];
+static int parentArr[205];
+static bool inPath[205], compVis[205];
+static int distArr[205], dist2Arr[205];
+static int q1[205], q2[205];
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
     cin >> n;
-    for(int i = 0; i < n; i++) adj[i].clear();
-    for(int i = 0, a, b; i < n-1; i++){
+    vector<vector<int>> adj(n);
+    for(int i=0,a,b;i<n-1;i++){
         cin >> a >> b;
         --a; --b;
         adj[a].push_back(b);
@@ -24,25 +23,26 @@ int main(){
     long long answer = 0;
     vector<int> path;
 
-    // For every pair of endpoints u < v
-    for(int u = 0; u < n; u++){
-        for(int v = u + 1; v < n; v++){
-            // 1) BFS from u until v to fill parentArr[]
-            for(int i = 0; i < n; i++) parentArr[i] = -1;
-            int qh = 0, qt = 0;
+    #pragma acc parallel loop collapse(2) reduction(max:answer)
+    for(int u=0; u<n; u++){
+        for(int v=0; v<n; v++){
+            if(v <= u) continue;
+
+            for(int i=0;i<n;i++) parentArr[i] = -1;
+            int h=0,t=0;
             parentArr[u] = u;
-            q[qt++] = u;
-            while(qh < qt && parentArr[v] == -1){
-                int x = q[ qh++ ];
-                for(int y: adj[x]){
+            q1[t++] = u;
+
+            while(h<t && parentArr[v] == -1){
+                int x = q1[h++];
+                for(int y : adj[x]){
                     if(parentArr[y] == -1){
                         parentArr[y] = x;
-                        q[qt++] = y;
+                        q1[t++] = y;
                     }
                 }
             }
 
-            // 2) Reconstruct the unique path u->...->v
             path.clear();
             int cur = v;
             while(parentArr[cur] != cur){
@@ -50,55 +50,51 @@ int main(){
                 cur = parentArr[cur];
             }
             path.push_back(u);
-            int pathLen = int(path.size()) - 1;
+            int pathLen = (int)path.size() - 1;
 
-            // 3) Mark those vertices removed
-            for(int i = 0; i < n; i++){
+            for(int i=0;i<n;i++){
                 inPath[i] = false;
                 compVis[i] = false;
             }
-            for(int x: path) inPath[x] = true;
+            for(int x : path) inPath[x] = true;
 
-            // 4) On the remaining forest compute the maximum diameter
             int bestDiam = 0;
-            for(int i = 0; i < n; i++){
+
+            for(int i=0;i<n;i++){
                 if(inPath[i] || compVis[i]) continue;
 
-                // BFS1: collect one component starting at i, find farthest 'far'
-                int head = 0, tail = 0;
-                q[tail++] = i;
+                int hh=0,tt=0;
+                q1[tt++] = i;
                 compVis[i] = true;
                 distArr[i] = 0;
-                vector<int> compNodes;
-                compNodes.push_back(i);
+                vector<int> comp;
+                comp.push_back(i);
                 int far = i;
 
-                while(head < tail){
-                    int x = q[head++];
+                while(hh<tt){
+                    int x = q1[hh++];
                     if(distArr[x] > distArr[far]) far = x;
-                    for(int y: adj[x]){
+                    for(int y : adj[x]){
                         if(!inPath[y] && !compVis[y]){
                             compVis[y] = true;
                             distArr[y] = distArr[x] + 1;
-                            q[tail++] = y;
-                            compNodes.push_back(y);
+                            q1[tt++] = y;
+                            comp.push_back(y);
                         }
                     }
                 }
 
-                // BFS2: from 'far' compute this component's diameter
-                // reset dist2Arr only on the nodes we will visit
-                for(int x: compNodes) dist2Arr[x] = -1;
+                for(int x : comp) dist2Arr[x] = -1;
 
-                int hh = 0, tt = 0;
+                hh=0; tt=0;
                 dist2Arr[far] = 0;
                 q2[tt++] = far;
                 int diam = 0;
 
-                while(hh < tt){
+                while(hh<tt){
                     int x = q2[hh++];
                     diam = max(diam, dist2Arr[x]);
-                    for(int y: adj[x]){
+                    for(int y : adj[x]){
                         if(!inPath[y] && dist2Arr[y] == -1){
                             dist2Arr[y] = dist2Arr[x] + 1;
                             q2[tt++] = y;
@@ -106,11 +102,11 @@ int main(){
                     }
                 }
 
-                bestDiam = max(bestDiam, diam);
+                if(diam > bestDiam) bestDiam = diam;
             }
 
-            // 5) maximize the product
-            answer = max(answer, 1LL * pathLen * bestDiam);
+            long long val = 1LL * pathLen * bestDiam;
+            if(val > answer) answer = val;
         }
     }
 
