e8b7989c3e2ebebea581f4459c0b11f0e7313c63
edmondium <nicko@uin-suska.ac.id>
2026-04-07 10:59:34 +0700

    Codeforces Beta Round 14

--- Letter.cpp
+++ Letter.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <openacc.h>
 using namespace std;
 
 int main() {
@@ -7,24 +8,24 @@ int main() {
 
     int n, m;
     cin >> n >> m;
+
     vector<string> g(n);
-    for (auto &row : g) 
-        cin >> row;
+    for (auto &s : g) cin >> s;
 
     int rmin = n, rmax = -1, cmin = m, cmax = -1;
+
+    #pragma acc parallel loop reduction(min:rmin,cmin) reduction(max:rmax,cmax)
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (g[i][j] == '*') {
-                rmin = min(rmin, i);
-                rmax = max(rmax, i);
-                cmin = min(cmin, j);
-                cmax = max(cmax, j);
+                rmin = rmin < i ? rmin : i;
+                rmax = rmax > i ? rmax : i;
+                cmin = cmin < j ? cmin : j;
+                cmax = cmax > j ? cmax : j;
             }
         }
     }
 
-    for (int i = rmin; i <= rmax; i++) {
-        cout << g[i].substr(cmin, cmax - cmin + 1) << "\n";
-    }
-    return 0;
+    for (int i : views::iota(rmin, rmax + 1))
+        cout << string_view(g[i]).substr(cmin, cmax - cmin + 1) << '\n';
 }
