02c46e17ae44b2455990e2a7e97f2927d5af4553
edmondium <nicko@uin-suska.ac.id>
2026-04-08 17:48:02 +0700

    Codeforces Beta Round 15

--- Triangles2.cpp
+++ Triangles2.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 static constexpr int64_t MOD = 1'000'000'009;
 
 int main() {
@@ -9,33 +8,30 @@ int main() {
 
     int64_t n;
     cin >> n;
-
-    // special case
     if (n == 2) {
         cout << 10 << "\n";
         return 0;
     }
 
-    // zb[i] = (3 + 2*zb[i-1]) % MOD
-    vector<int64_t> zb(n + 1);
+    int64_t* zb = new int64_t[n+1];
     zb[0] = 1;
-    for (int i = 1; i <= n; ++i) {
+
+    #pragma acc parallel loop copy(zb[0:n+1])
+    for (int i = 1; i <= n; ++i)
         zb[i] = (3 + 2 * zb[i - 1]) % MOD;
-    }
 
-    // build ans exactly like your original loop
-    int64_t ans = 0;
     int64_t cur = 1;
-    for (int64_t fd = 3; fd <= n; ++fd) {
-        if (fd & 1) {
-            int64_t idx = ((fd - 1) >> 1) - 1;
-            if (idx >= 0)
-                cur = cur * zb[idx] % MOD;
-        }
-        ans = (ans + 2 * cur) % MOD;
-    }
+    auto seq = views::iota(int64_t(3), n + 1);
 
-    // final magic formula
+    int64_t ans = ranges::fold_left(seq, int64_t(0),
+        [&](int64_t acc, int64_t fd) {
+            if (fd & 1) {
+                int64_t idx = ((fd - 1) >> 1) - 1;
+                if (idx >= 0) cur = cur * zb[idx] % MOD;
+            }
+            return (acc + 2 * cur) % MOD;
+        });
+
     ans = (
         (2 * (((ans + 2) % MOD) * ans % MOD)) % MOD
       + (ans * 4) % MOD
@@ -43,5 +39,5 @@ int main() {
     ) % MOD;
 
     cout << ans << "\n";
-    return 0;
+    delete[] zb;
 }
