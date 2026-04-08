2133d493a4a0e14410965cbd1154d4458c72b77c
edmondium <nicko@uin-suska.ac.id>
2026-04-08 15:43:18 +0700

    Codeforces Beta Round 15

--- CottageVillage.cpp
+++ CottageVillage.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
+#include <ranges>
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n, t;
     cin >> n >> t;
     vector<pair<int,int>> H(n);
-    for(auto &h : H){
-        cin >> h.first >> h.second;
-        h.first *= 2;      // work in “double” space
-        // h.second is side length; half-side in double space = h.second
+    for (auto& [x,a] : H) {
+        cin >> x >> a;
+        x *= 2;
     }
-    t *= 2;                // required gap in double space
+    t *= 2;
 
-    sort(H.begin(), H.end());
+    ranges::sort(H);
 
-    int ans = 2;  // one on the far left, one on the far right
-    for(int i = 0; i + 1 < n; i++){
+    int ans = 2;
+    #pragma acc parallel loop reduction(+:ans)
+    for (int i = 0; i < n - 1; i++) {
         int right_i = H[i].first + H[i].second;
         int left_j  = H[i+1].first - H[i+1].second;
         int gap = left_j - right_i;
-        if(gap == t)      ans += 1;  // exactly fits touching both
-        else if(gap > t)  ans += 2;  // can slide to touch either
+        if (gap == t) ans += 1;
+        else if (gap > t) ans += 2;
     }
 
-    cout << ans << "\n";
-    return 0;
+    cout << ans << '\n';
 }
