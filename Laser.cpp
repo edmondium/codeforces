3d1e14e2cd0ff3db528500211c794dad1f58a6dd
edmondium <nicko@uin-suska.ac.id>
2026-04-08 16:32:24 +0700

    Codeforces Beta Round 15

--- Laser.cpp
+++ Laser.cpp
@@ -8,26 +8,33 @@ int main() {
 
     int t;
     cin >> t;
-    while (t--) {
-        ll n, m, x1, y1, x2, y2;
-        cin >> n >> m >> x1 >> y1 >> x2 >> y2;
 
-        ll a = llabs(x1 - x2);
-        ll b = llabs(y1 - y2);
+    ll *n = new ll[t];
+    ll *m = new ll[t];
+    ll *x1 = new ll[t];
+    ll *y1 = new ll[t];
+    ll *x2 = new ll[t];
+    ll *y2 = new ll[t];
+    ll *results = new ll[t];
 
-        // Number of distinct columns/rows each laser can cover
-        ll W = n - a;
-        ll H = m - b;
-
-        // Overlap of their swept rectangles
-        ll overlapW = max(0LL, n - 2*a);
-        ll overlapH = max(0LL, m - 2*b);
-
-        ll melted = 2 * W * H - overlapW * overlapH;
-        ll total = n * m;
-        ll unmelted = total - melted;
-
-        cout << unmelted << '\n';
+    for (int i = 0; i < t; i++) {
+        cin >> n[i] >> m[i] >> x1[i] >> y1[i] >> x2[i] >> y2[i];
     }
-    return 0;
+
+    #pragma acc parallel loop copyin(n[0:t],m[0:t],x1[0:t],y1[0:t],x2[0:t],y2[0:t]) copyout(results[0:t])
+    for (int i = 0; i < t; i++) {
+        ll a = llabs(x1[i] - x2[i]);
+        ll b = llabs(y1[i] - y2[i]);
+        ll W = n[i] - a;
+        ll H = m[i] - b;
+        ll overlapW = max(0LL, n[i] - 2*a);
+        ll overlapH = max(0LL, m[i] - 2*b);
+        ll melted = 2 * W * H - overlapW * overlapH;
+        ll total = n[i] * m[i];
+        results[i] = total - melted;
+    }
+
+    for (int i = 0; i < t; i++) cout << results[i] << '\n';
+
+    delete[] n; delete[] m; delete[] x1; delete[] y1; delete[] x2; delete[] y2; delete[] results;
 }
