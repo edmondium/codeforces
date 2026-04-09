65b63e4f3bea802c5bc21bebf014470ec934901a
edmondium <nicko@uin-suska.ac.id>
2026-04-09 15:14:18 +0700

    Codeforces Beta Round 16

--- Logging.cpp
+++ Logging.cpp
@@ -1,58 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int toMinutes(int h, int m, bool isPM) {
-    if (!isPM && h == 12) h = 0;
-    if ( isPM && h < 12) h += 12;
-    return h * 60 + m;
-}
+auto toMinutes = [](int h,int m,bool pm){
+    return (!pm && h==12?0:(pm && h<12?h+12:h))*60+m;
+};
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
-    string line;
-    getline(cin, line); // consume newline
+    int n; cin>>n;
+    string line; getline(cin,line);
 
-    int days = 1;
-    int prevTime = -1;
-    int sameCount = 0;
+    vector<int> times(n);
+    transform(begin(times),end(times),begin(times),[&](int){
+        getline(cin,line);
+        auto ts=line.substr(1,line.find(']')-1);
+        int hh=stoi(ts.substr(0,2)),mm=stoi(ts.substr(3,2));
+        bool pm=ts.find("p.m.")!=string::npos;
+        return toMinutes(hh,mm,pm);
+    });
 
-    for(int i = 0; i < n; i++){
-        getline(cin, line);
-        auto close = line.find(']');
-        string ts = line.substr(1, close - 1);  // "hh:mm x.m."
-
-        int hh = stoi(ts.substr(0,2));
-        int mm = stoi(ts.substr(3,2));
-        bool isPM = (ts[6] == 'p');
-
-        int currTime = toMinutes(hh, mm, isPM);
-
-        if (currTime < prevTime) {
-            // midnight rollover
-            days++;
-            sameCount = 1;
-        }
-        else if (currTime == prevTime) {
-            // repeated timestamp
-            sameCount++;
-            if (sameCount > 10) {
-                // too many logs in one minute → new day
-                days++;
-                sameCount = 1;
-            }
-        }
-        else {
-            // strictly later time
-            sameCount = 1;
-        }
-
-        prevTime = currTime;
+    int days=1,prev=-1,cnt=0;
+    #pragma acc parallel loop reduction(+:days)
+    for(int i=0;i<n;i++){
+        if(times[i]<prev){days++;cnt=1;}
+        else if(times[i]==prev){if(++cnt>10){days++;cnt=1;}}
+        else cnt=1;
+        prev=times[i];
     }
-
-    cout << days << "\n";
-    return 0;
+    cout<<days<<"\n";
 }
