af304deb949b77f9faabfb795366ca45ea36c7ea
edmondium <nicko@uin-suska.ac.id>
2026-04-10 09:06:24 +0700

    Codeforces Beta Round 17

--- NoldbachProblem.cpp
+++ NoldbachProblem.cpp
@@ -10,25 +10,27 @@ int main() {
 
     vector<bool> isPrime(n+1, true);
     isPrime[0] = isPrime[1] = false;
-    for (int p = 2; p * p <= n; ++p) {
-        if (!isPrime[p]) continue;
-        for (int j = p * p; j <= n; j += p)
-            isPrime[j] = false;
-    }
 
-    vector<int> primes;
-    for (int i = 2; i <= n; ++i) {
-        if (isPrime[i]) primes.push_back(i);
-    }
-
-    int count = 0;
-    for (size_t i = 0; i + 1 < primes.size(); ++i) {
-        int s = primes[i] + primes[i+1] + 1;
-        if (s <= n && isPrime[s]) {
-            count++;
+    int limit = (int)std::sqrt(n);
+    #pragma acc parallel loop
+    for (int p = 2; p <= limit; ++p) {
+        if (isPrime[p]) {
+            for (int j = p * p; j <= n; j += p)
+                isPrime[j] = false;
         }
     }
 
+    vector<int> primes;
+    for (int i : std::views::iota(2, n+1))
+        if (isPrime[i]) primes.push_back(i);
+
+    int m = (int)primes.size() - 1;
+    int count = 0;
+    #pragma acc parallel loop reduction(+:count)
+    for (int idx = 0; idx < m; ++idx) {
+        int s = primes[idx] + primes[idx+1] + 1;
+        if (s <= n && isPrime[s]) count++;
+    }
+
     cout << (count >= k ? "YES\n" : "NO\n");
-    return 0;
 }
